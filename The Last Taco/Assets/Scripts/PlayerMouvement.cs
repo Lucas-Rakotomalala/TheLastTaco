b5d48f2ff9f78ed10ec55f006b3602e30f2931c3
Lucas Rakotomalala <lucas.rakotomalala@etu.univ-lyon1.fr>
2020-09-23 20:31:19 +0200

    Nouvelle scène + première animation

---
+++ The Last Taco/Assets/Scripts/PlayerMouvement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerMouvement : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+
+    private Vector3 velocity= Vector3.zero;
+   
+     void FixedUpdate()
+    {
+       float horizontalMovement = Input.GetAxis("Horizontal")*moveSpeed*Time.deltaTime;
+
+       MovePlayer(horizontalMovement);
+
+       Flip(rb.velocity.x);
+
+       float characterVelocity = Mathf.Abs(rb.velocity.x); 
+       animator.SetFloat("Speed",characterVelocity);
+    }
+
+    void MovePlayer(float _horizontalMouvement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMouvement,rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity,targetVelocity,ref velocity, .05f);
+    }
+
+    void Flip(float _velocity)
+    {
+        if(_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }else if (_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        } 
+    }
+}
