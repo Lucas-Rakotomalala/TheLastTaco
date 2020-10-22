70535ff72d59824772ec6aefeac51325b3e3c7c9
Alexis <alexis.bonis@etu.univ-lyon1.fr>
2020-10-22 12:13:54 +0200

    ajout règles

--- The Last Taco/Assets/MainMenu.cs
+++ The Last Taco/Assets/MainMenu.cs
@@ -6,15 +6,26 @@ public class MainMenu : MonoBehaviour
     //public string levelToLoad="Scenes/SampleScene";
     
     public GameObject settingsWindow;
+    public GameObject RulesWindow;
     
     public void StartGame()
     {
         SceneManager.LoadScene("Scenes/SampleScene");
     }
     
+    public void RulesButton()
+    {
+        RulesWindow.SetActive(true);
+    }
+    
     public void SettingsButton()
     {
         settingsWindow.SetActive(true);
+    }
+    
+    public void CloseRulesWindow()
+    {
+        RulesWindow.SetActive(false);
     }
     
     public void CloseSettingsWindow()
